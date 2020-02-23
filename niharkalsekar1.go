ae5c68734030d2ad0634d2ad7ea08045e864b63e
niharkalsekar <2017.nihar.kalsekar@ves.ac.in>
2020-02-23 20:22:43 +0530

    1.0

---
+++ niharkalsekar1.go
@@ -0,0 +1,11 @@
+package niharkalsekar1
+import "fmt"
+import "math"
+func Square(num int){
+res:=num*num
+fmt.Println("square=",res)
+}
+func SquareRoot(num int){
+res:=math.Sqrt(float64(num))
+fmt.Println("squareroot=",res)
+}
